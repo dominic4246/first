f3dcdbc14dffd59a9a9cf885527979506cff0960
Dominic Hahm <dominichahm@Dominics-MacBook-Pro.local>
2022-08-07 16:49:00 +0900

    token1

---
+++ git.py
@@ -0,0 +1,14 @@
+# gift writing process #
+
+# 1. initialize the set-up
+# 2. add all the gifts into one box
+# 3. write message that describes the content
+# 4. push the gift with address
+# 5. confirmation (1st time only)
+
+
+
+# github commmands for pushing #
+
+
+print("hello")
